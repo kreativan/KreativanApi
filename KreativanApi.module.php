e98d2451b81edf4d4b7055b6bf05fcd6f6487bae
Ivan Milincic <kreativan@outlook.com>
2019-07-07 21:39:09 +0200

    add getUserIP() method

--- KreativanApi.module.php
+++ KreativanApi.module.php
@@ -11,6 +11,7 @@
  *  writeToFile() 
  *  getFolders($dir)
  *  uplaodFile()
+ *	getUserIP()
  *	
  *	Module:
  *	moduleSettings() - chanage module settings
@@ -142,6 +143,33 @@ class KreativanApi extends WireData implements Module {
         return file_put_contents($file, $current);
     }
     
+	
+	/**
+     *  Get Visitor IP
+     *
+     */
+    function getUserIP() {
+        // Get real visitor IP behind CloudFlare network
+        if (isset($_SERVER["HTTP_CF_CONNECTING_IP"])) {
+            $_SERVER['REMOTE_ADDR'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
+            $_SERVER['HTTP_CLIENT_IP'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
+        }
+        $client  = @$_SERVER['HTTP_CLIENT_IP'];
+        $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+        $remote  = $_SERVER['REMOTE_ADDR'];
+
+        if(filter_var($client, FILTER_VALIDATE_IP)) {
+            $ip = $client;
+        }
+        elseif(filter_var($forward, FILTER_VALIDATE_IP)) {
+            $ip = $forward;
+        }
+        else {
+            $ip = $remote;
+        }
+
+        return $ip;
+    }
 	
 	/* ==================================================================================
         API Methods
