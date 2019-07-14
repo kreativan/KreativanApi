a8f01e148f2926e9bc41eb8c13ff15a3df69cafd
Ivan Milincic <kreativan@outlook.com>
2019-07-14 22:24:07 +0200

    remove getUserIP() method, processwire already has $session->getIP()

--- KreativanApi.module.php
+++ KreativanApi.module.php
@@ -11,7 +11,6 @@
  *  writeToFile() 
  *  getFolders($dir)
  *  uplaodFile()
- *	getUserIP()
  *	
  *	Module:
  *	moduleSettings() - chanage module settings
@@ -142,34 +141,6 @@ class KreativanApi extends WireData implements Module {
         $current .= $what;
         // Write the contents back to the file
         return file_put_contents($file, $current);
-    }
-    
-	
-	/**
-     *  Get Visitor IP
-     *
-     */
-    public function getUserIP() {
-        // Get real visitor IP behind CloudFlare network
-        if (isset($_SERVER["HTTP_CF_CONNECTING_IP"])) {
-            $_SERVER['REMOTE_ADDR'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
-            $_SERVER['HTTP_CLIENT_IP'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
-        }
-        $client  = @$_SERVER['HTTP_CLIENT_IP'];
-        $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
-        $remote  = $_SERVER['REMOTE_ADDR'];
-
-        if(filter_var($client, FILTER_VALIDATE_IP)) {
-            $ip = $client;
-        }
-        elseif(filter_var($forward, FILTER_VALIDATE_IP)) {
-            $ip = $forward;
-        }
-        else {
-            $ip = $remote;
-        }
-
-        return $ip;
     }
 	
 	/* ==================================================================================
